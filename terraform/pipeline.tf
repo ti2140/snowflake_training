3f8e227a56883befe96d3ace53e5deb27c1dee2c
ti2140 <ti.tube140@gmail.com>
2026-06-12 13:59:11 +0900

    fix

--- terraform/pipeline.tf
+++ terraform/pipeline.tf
@@ -1,7 +1,7 @@
 # ==========================================
 # Storage Integration定義
 # ==========================================
-resource "snowflake_storage_integration" "s3_int" {
+resource "snowflake_storage_integration" "s3_int_v2" {
   name                 = "S3_INT"
   type                 = "EXTERNAL_STAGE"
   enabled              = true
