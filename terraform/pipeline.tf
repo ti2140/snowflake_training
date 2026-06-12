9a67de38ba79c7046afee71cc9e290aec8470616
ti2140 <ti.tube140@gmail.com>
2026-06-12 14:01:14 +0900

    a

--- terraform/pipeline.tf
+++ terraform/pipeline.tf
@@ -44,7 +44,7 @@ resource "snowflake_stage" "st_s3_mail" {
   schema              = snowflake_schema.training_raw.name
   name                = "ST_S3_MAIL"
   url                 = var.s3_bucket_url
-  storage_integration = snowflake_storage_integration.s3_int.name
+  storage_integration = snowflake_storage_integration.s3_int_v2.name
   file_format         = "FORMAT_NAME = ${snowflake_database.training_db.name}.${snowflake_schema.training_raw.name}.${snowflake_file_format.mail_jsonl_format.name}"
   comment             = "External stage for mail data from S3."
 }
@@ -161,7 +161,7 @@ resource "snowflake_grant_privileges_to_account_role" "s3_int_usage" {
   privileges        = ["USAGE"]
   on_account_object {
     object_type = "INTEGRATION"
-    object_name = snowflake_storage_integration.s3_int.name
+    object_name = snowflake_storage_integration.s3_int_v2.name
   }
 }
 
